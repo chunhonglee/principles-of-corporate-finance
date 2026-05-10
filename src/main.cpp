61701f3900185437e8d7c22dd1890b2e936c4e49
James Lee <c.h.lee@student.tudelft.nl>
2026-05-10 18:59:24 +0200

    Implemented Future Value (FV), Present value (PV) and Discounted Cash Flow (DCF) calculations

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
+#include <cmath>
 #include "module/example.h"
-#include <boost/version.hpp>
-// #include "raylib.h"
+#include <vector>
+
+namespace Value {
+    float calculateFutureValue(float presentValue, float r, int t) {
+        float futureValue = presentValue * pow((1 + r), t);
+        
+        return futureValue;
+    }
+
+    float calculateDiscountFactor(float r, int t) {
+        float discountFactor = 1 / pow((1 + r), t);
+
+        return discountFactor;
+    }
+
+    float calculatePresentValue(float C, float r, int t) {
+        float discountFactor = calculateDiscountFactor(r, t);
+        float presentValue = C * discountFactor;
+
+        return presentValue;
+    }
+
+    float calculateNetPresentValue(float presentValue, float investment) {
+        float netPresentValue = presentValue - investment;
+
+        return netPresentValue;
+    }
+
+    float calculateDiscountedCashFlow(std::vector<float> C, float r, int T) {
+        float presentValue = 0;
+        for (int t = 1; t <= T; t++) {
+            presentValue += calculatePresentValue(C[t-1], r, t);
+        }
+
+        return presentValue;
+    }
+
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, from main!\n";
-    Example::printExample();
-
-    std::cout << "Boost version: "
-              << BOOST_VERSION / 100000 << "."
-              << BOOST_VERSION / 100 % 1000 << "."
-              << BOOST_VERSION % 100
-              << std::endl;
-
-    // InitWindow(800, 450, "raylib [core] example - basic window");
-
-    // while (!WindowShouldClose())
-    // {
-    //     BeginDrawing();
-    //         ClearBackground(RAYWHITE);
-    //         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-    //     EndDrawing();
-    // }
-
-    // CloseWindow();
+    float futureValue = Value::calculateFutureValue(100, 0.07, 2);
+    float presentValue = Value::calculatePresentValue(114.49, 0.07, 2);
+    float discountedCashFlow = Value::calculateDiscountedCashFlow({30000, 870000}, 0.12, 2);
+    std::cout << futureValue << "\n";
+    std::cout << presentValue << "\n";
+    std::cout << discountedCashFlow << "\n";
 
     return 0;
 }
