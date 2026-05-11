2a382be91ebeedbec4f8c2b4fbd5d4df3323f79b
James Lee <c.h.lee@student.tudelft.nl>
2026-05-11 17:23:43 +0200

    Implemented Perpetuities and Annuities

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,8 @@ namespace Value {
         return futureValue;
     }
 
-    float calculateDiscountFactor(float r, int t) {
-        float discountFactor = 1 / pow((1 + r), t);
-
-        return discountFactor;
-    }
-
     float calculatePresentValue(float C, float r, int t) {
-        float discountFactor = calculateDiscountFactor(r, t);
+        float discountFactor = 1 / pow((1 + r), t);
         float presentValue = C * discountFactor;
 
         return presentValue;
@@ -29,7 +23,8 @@ namespace Value {
         return netPresentValue;
     }
 
-    float calculateDiscountedCashFlow(std::vector<float> C, float r, int T) {
+    float calculateDiscountedCashFlow(std::vector<float> C, float r) {
+        int T = C.size();
         float presentValue = 0;
         for (int t = 1; t <= T; t++) {
             presentValue += calculatePresentValue(C[t-1], r, t);
@@ -38,15 +33,64 @@ namespace Value {
         return presentValue;
     }
 
+    float calculatePerpetuityPresentValue(float C, float r, bool due = false) {
+        float perpetuity = C / r;
+        if (due == true) {
+            perpetuity *= (1 + r);
+        }
+
+        return perpetuity;
+    }
+
+    float calculateAnnuityPresentValue(float C, float r, int t, bool due = false) {
+        float discountFactor = 1 / pow((1 + r), t);
+        float annuity = C/r - discountFactor * C / r;
+        if (due == true) {
+            annuity *= (1 + r);
+        }
+
+        return annuity;
+    }
+
+    float calculatePerpetuityFutureValue() {
+        float perpetuityFutureValue = std::numeric_limits<float>::infinity();
+
+        return perpetuityFutureValue;
+    }
+
+    float calculateAnnuityFutureValue(float C, float r, int t, bool due = false) {
+        float annuity = calculateAnnuityPresentValue(C, r, t, due);
+        float annuityFutureValue = annuity * pow(1 + r, t);
+
+        return annuityFutureValue;
+    }
+
 }
 
 int main(int argc, char** argv) {
-    float futureValue = Value::calculateFutureValue(100, 0.07, 2);
-    float presentValue = Value::calculatePresentValue(114.49, 0.07, 2);
-    float discountedCashFlow = Value::calculateDiscountedCashFlow({30000, 870000}, 0.12, 2);
-    std::cout << futureValue << "\n";
-    std::cout << presentValue << "\n";
-    std::cout << discountedCashFlow << "\n";
+    // float futureValue = Value::calculateFutureValue(100, 0.07, 2);
+    // std::cout << futureValue << "\n";
+
+    // float presentValue = Value::calculatePresentValue(114.49, 0.07, 2);
+    // std::cout << presentValue << "\n";
+
+    // float discountedCashFlow = Value::calculateDiscountedCashFlow({30000, 870000}, 0.12);
+    // std::cout << discountedCashFlow << "\n";
+
+    // float perpetuity = Value::calculatePerpetuityPresentValue(1000, 0.1);
+    // std::cout << perpetuity << "\n";
+    // float perpetuityDue = Value::calculatePerpetuityPresentValue(1000, 0.1, true);
+    // std::cout << perpetuityDue << "\n";
+
+    // float annuity = Value::calculateAnnuityPresentValue(5000, 0.07, 5);
+    // std::cout << annuity << "\n";
+    // float annuityDue = Value::calculateAnnuityPresentValue(5000, 0.07, 5, true);
+    // std::cout << annuityDue << "\n";
+
+    float perpetuityFutureValue = Value::calculatePerpetuityFutureValue();
+    std::cout << perpetuityFutureValue << "\n";
+    float annuityFutureValue = Value::calculateAnnuityFutureValue(20, 0.08, 5);
+    std::cout << annuityFutureValue << "\n";
 
     return 0;
 }
